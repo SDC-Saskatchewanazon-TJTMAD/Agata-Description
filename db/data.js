74ae9d9acdb754e4896010bede46135f9389406f
agatadrozdowska <agata.drozdowska@icloud.com>
2020-02-13 21:52:12 -0600

    Generating data

---
+++ db/data.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const DataSchema = new Schema(
+  {
+    product_id: Number,
+    productName: String,
+    productDescription: String,
+    price: Number,
+    category_id: Number,
+    rating: Number
+  },
+  { collection: "description" }
+);
+
+module.exports = mongoose.model("Data", DataSchema);
---
+++ db/seed.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const faker = require("faker");
+const db = require("./index.js");
+
+const collection = db.collection("description");
+
+const pushBatch = (countSoFar, target) => {
+  console.log("countSofar: " + countSoFar);
+  let descriptions = [];
+  for (let i = 0; i < 1000; i++) {
+    descriptions.push({
+      product_Id: i + countSoFar,
+      productName: faker.commerce.productName(),
+      productDescription: faker.lorem.words(30),
+      price: faker.commerce.price(),
+      category_id: faker.random.number({ min: 1, max: 10 }),
+      rating: faker.random.number({ min: 1, max: 5 })
+    });
+  }
+  if (countSoFar < target) {
+    collection
+      .insertMany(descriptions)
+      .then(() => pushBatch(countSoFar + 1000, target));
+  }
+};
+
+pushBatch(0, 10000000);
